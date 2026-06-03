13fc09eba70186eae762beaca2382347183ae9d8
Ray Liu <ray.liu@unity.cn>
2026-06-03 10:32:29 +0800

    rotating obj speed

--- projects/GaussianExample-URP/Assets/Scripts/RotatingObject.cs
+++ projects/GaussianExample-URP/Assets/Scripts/RotatingObject.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class RotatingObject : MonoBehaviour
 {
+    public float rotating_speed = 1f;
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.up, 1f,Space.Self);
+        this.transform.Rotate(Vector3.up, rotating_speed,Space.Self);
     }
 }
